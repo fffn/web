b127317d06eecb241750347cb326a259ba5e96ec
af <alesfrydrych@seznam.cz>
2020-07-13 22:54:53 +0200

    Initial commit

---
+++ src/assets/js/functions.js
@@ -0,0 +1,90 @@
+// Menu collapse vanilla JS
+function menuCollapse() {
+    let links = document.querySelectorAll(".navigation__link");
+
+    links.forEach((link) => {
+        link.addEventListener("click", () => {
+        document.querySelector(".navigation__checkbox").checked = false;
+        });
+    });
+}
+
+
+// Back To Top button
+function initBackToTop() {
+    var pxShow = 600;
+    var scrollSpeed = 500;
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= pxShow) {
+            $("#backtotop").addClass('visible');
+        } else {
+            $("#backtotop").removeClass('visible');
+        }
+    });
+    $('#backtotop a').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, scrollSpeed);
+        return false;
+    });
+}
+
+
+// ScrollReveal
+function initScrollReveal() {
+    window.sr = ScrollReveal();
+
+        // Simple reveal
+        sr.reveal('.is-title-reveal', {
+            origin: 'bottom',
+            distance: '20px',
+            duration: 600,
+            delay: 100,
+            rotate: { x: 0, y: 0, z: 0 },
+            opacity: 0,
+            scale: 1,
+            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+            container: window.document.documentElement,
+            mobile: true,
+            reset: false,
+            useDelay: 'always',
+            viewFactor: 0.2,
+
+        });
+
+        // Revealing features
+        sr.reveal('.is-feature-reveal', {
+            origin: 'bottom',
+            distance: '20px',
+            duration: 600,
+            delay: 100,
+            rotate: { x: 0, y: 0, z: 0 },
+            opacity: 0,
+            scale: 1,
+            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+            container: window.document.documentElement,
+            mobile: true,
+            reset: true,
+            useDelay: 'always',
+            viewFactor: 0.2,
+
+        }, 160);
+
+        // Revealing features
+        sr.reveal('.is-icon-reveal', {
+            origin: 'bottom',
+            distance: '20px',
+            duration: 600,
+            delay: 100,
+            rotate: { x: 0, y: 0, z: 0 },
+            opacity: 0,
+            scale: 1,
+            easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+            container: window.document.documentElement,
+            mobile: true,
+            reset: false,
+            useDelay: 'always',
+            viewFactor: 0.2,
+
+        }, 160);
+}
